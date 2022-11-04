68e2327ff0da5b818e940e129ee11c1d47676bc7
samsnwn <samuel.c.glantz@gmail.com>
2022-11-04 14:18:23 +0100

    repo created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { bankOne, bankTwo } from "./api";
+import { useState } from "react";
 
 function App() {
+  const [bank, setBank] = useState(true);
+  const [power, setPower] = useState(false);
+  const [display, setDisplay] = useState();
+  const [volume, setVolume] = useState(50);
+  const [volumeDisplay, setVolumeDisplay] = useState()
+
+  
+    const volumeHandler = (e) => {
+      setVolume(e.target.value)
+      setDisplay();
+      setVolumeDisplay(`Volume: ${e.target.value}`);
+      setTimeout(() => {
+        setVolumeDisplay();
+      }, 2000);
+    };
+
+  const playHandler = (e) => {
+
+    const triggers = bankOne.map((el) =>el.keyTrigger);
+
+    if (e.key && triggers.some((item) => e.key.toUpperCase() === item)  && power)  {
+      if (bank) {
+        const obj = bankOne.filter(
+          (x) => e.key.toUpperCase() === x.keyTrigger)[0]
+        const audio = new Audio(obj.url);
+        setVolumeDisplay();
+        setDisplay(obj.id);
+        audio.volume = volume/100
+        audio.play()
+      } else {
+        const obj = bankTwo.filter(
+          (x) => e.key.toUpperCase() === x.keyTrigger)[0]
+        const audio = new Audio(obj.url);
+        setVolumeDisplay()
+        setDisplay(obj.id)
+        audio.volume = volume/100
+        audio.play()
+      }
+    } 
+    else if (e.target.id && triggers.some(item=> e.target.id === item)  && power) {
+      if (bank) {
+        const obj = bankOne.filter(
+          (x) => e.target.id === x.keyTrigger)[0];
+        const audio = new Audio(obj.url);
+        setVolumeDisplay();
+        setDisplay(obj.id);
+        audio.volume = volume/100
+        audio.play()
+      } else {
+        const obj = bankTwo.filter(
+          (x) => e.target.id === x.keyTrigger)[0];
+        const audio = new Audio(obj.url);
+        setVolumeDisplay();
+        setDisplay(obj.id);
+        audio.volume = volume/100
+        audio.play()
+      }
+    }
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={(e) => playHandler(e)} onKeyDown={(e) => playHandler(e)} tabIndex={-1}>
+      <div id="drum-machine">
+        <div id="drum-pads">
+          {bank
+            ? bankOne.map((el, i) => (
+                <div
+                  className="drum-pad"
+                  id={el.keyTrigger}
+                  name={el.id}
+                  key={i}
+                >
+                  {el.keyTrigger}
+                </div>
+              ))
+            : bankTwo.map((el, i) => (
+                <div
+                  className="drum-pad"
+                  id={el.keyTrigger}
+                  name={el.id}
+                  key={i}
+                >
+                  {el.keyTrigger}
+                </div>
+              ))}
+        </div>
+        <div id="controls">
+          <div className="power">
+            <label>Power</label>
+            <input
+              type="checkbox"
+              onChange={() => setPower((pre) => !pre)}
+            ></input>
+          </div>
+          <div id="display">
+            {display}
+            {volumeDisplay}
+          </div>
+          <input type="range" onChange={volumeHandler} min="0" max="100" value={volume }></input>
+          <div className="bank">
+            <label>Bank</label>
+            <input
+              type="checkbox"
+              onChange={() => setBank((pre) => !pre)}
+            ></input>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
